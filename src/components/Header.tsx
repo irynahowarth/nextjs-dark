92dddbc9e8f7e9e7328122d19c3f7b399a74c462
Iryna Howarth <irynahowarth@gmail.com>
2024-05-07 15:27:18 +0100

    [feature]: add ButtonLink for Header and style/data for Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
-import WordMark from "@/components/WordMark";
+import NavBar from "@/components/NavBar";
+import { createClient } from "@/prismicio";
 
-export default function Header() {
+
+export default async function Header() {
+  const client = createClient();
+  const setting = await client.getSingle('settings')
   return (
-    <div><WordMark /></div>
+    <header>
+      <NavBar settings={setting} />
+    </header>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,46 @@
+'use client';
+import WordMark from "@/components/WordMark";
+import { Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+import ButtonLink from "@/components/ButtonLink";
+
+type NavBarProps = {
+  settings: Content.SettingsDocument;
+}
+
+export default function NavBar({settings}:NavBarProps) {
+  return (
+    <nav aria-label="Main" className="px-4 py-4 md:px-6 md:py-6">
+      <div className="mx-auto max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center flex">
+        <Link href='/'>
+          <WordMark />
+          <span className="sr-only">Glisten.ai Home page</span>
+        </Link>
+          <ul className="flex gap-6">
+          {settings.data.navigation.map((item) => {
+            if (item.cta_button) {
+              return (
+                <li key={item.label}>
+                  <ButtonLink
+                  key={item.label}
+                  field={item.link}>
+                  {item.label}
+                    </ButtonLink>
+                </li>
+              )
+            }
+            return (
+              <li key={item.label}>
+                <PrismicNextLink field={item.link}
+                  className="inline-flex min-h-11 items-center"
+                >
+                  {item.label}
+                </PrismicNextLink>
+              </li>
+          )})}
+          </ul>
+        </div>
+    </nav>
+  )
+}
