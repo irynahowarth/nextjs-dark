93a5f9aeba02ce5708ae5aded6b0ed10e868d331
Iryna Howarth <irynahowarth@gmail.com>
2024-05-13 14:51:27 +0100

    [feature]: add GSAP and use it for Hero slice animation

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,6 +4,7 @@ import StarGrid from "@/components/StarGrid";
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+import AnimatedComponent from "./AnimatedComponent";
 
 /**
  * Props for `Hero`.
@@ -21,27 +22,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="text-center"
     >
-      <div className="relative">
-        <StarGrid />
-        {isFilled.richText(slice.primary.heading) && (
-          <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
-            <PrismicText field={slice.primary.heading} />
-          </h1>
-        )}
-        {isFilled.richText(slice.primary.body) && (
-          <div className="mx-auto mt-6 max-w-md text-balance text-slate-300">
-            <PrismicRichText field={slice.primary.body} />
-            </div>
-        )}
-        {isFilled.link(slice.primary.button_link) && (
-          <ButtonLink className="mt-8" field={slice.primary.button_link}>{slice.primary.button_label}</ButtonLink>)}
-        {isFilled.image(slice.primary.image) && (
-          <div className="glass-container mt-16 w-fit">
-            <div className="absolute inset-0 -z-10 bg-blue-500/30  blur-2xl filter"/>
-            <PrismicNextImage field={slice.primary.image} className="rounded-lg"/>
-          </div>
-        )}
-      </div>
+      <AnimatedComponent slice={slice} />
     </Bounded>
   );
 };
